39a9bc16a2ed89bf4f9e68f24f3238f93ad46bf4
Ryan <ryan.ferreira@novigisolution.com.br>
2024-05-09 13:44:37 -0300

    A2.2

--- core/crud.js
+++ core/crud.js
@@ -1,9 +1,9 @@
-const fs = require("fs");
-const DB_FILE_PATH = "./core/db";
-
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var fs_1 = require("fs");
+var DB_FILE_PATH = "./core/db";
 function create(content) {
-  fs.writeFileSync(DB_FILE_PATH, content);
+    fs_1.default.writeFileSync(DB_FILE_PATH, content);
 }
-
 //SIMULATION
 create("Gravar a aula");
