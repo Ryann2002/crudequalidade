d467fe433507bb49f73740d532a9466a14a3a264
Ryan <ryan.ferreira@novigisolution.com.br>
2024-05-09 15:20:20 -0300

    A2.4

--- core/crud.ts
+++ core/crud.ts
@@ -1,5 +1,7 @@
 import { clear } from "console";
+import { randomUUID } from "crypto";
 import fs from "fs";
+import { todo } from "node:test";
 
 const DB_FILE_PATH = "./core/db";
 console.log("[CRUD]");
@@ -8,14 +10,21 @@ function convertToJSON(content: Object) {
   return JSON.stringify(content, null, 2);
 }
 
+type UUID = string;
 interface Todo {
+  id: UUID;
   date: string;
   content: string;
   done: boolean;
 }
 
-function create(content: string) {
+function writeOnDB(todos: Array<Todo>) {
+  fs.writeFileSync(DB_FILE_PATH, convertToJSON({ todos: todos }));
+}
+
+function create(content: string): Todo {
   const todo: Todo = {
+    id: randomUUID(),
     content: content,
     done: false,
     date: new Date().toISOString(),
@@ -23,7 +32,9 @@ function create(content: string) {
 
   const todos: Array<Todo> = [...read(), todo];
 
-  fs.writeFileSync(DB_FILE_PATH, convertToJSON({ todos: todos }));
+  writeOnDB(todos);
+
+  return todo;
 }
 
 function read(): Array<Todo> {
@@ -35,16 +46,53 @@ function read(): Array<Todo> {
   }
 
   return db.todos;
-
-  return db;
 }
 
-function clearDB() {
+function deleteById(id: UUID) {
+  const todos = read();
+
+  const filteredTodos = todos.filter((todo) => todo.id != id);
+
+  console.log(filteredTodos);
+
+  if (!filteredTodos) {
+    throw new Error("Id not found");
+  }
+  writeOnDB(filteredTodos);
+}
+
+function update(id: UUID, partialTodo: Partial<Todo>): Todo {
+  let updatedTodo;
+  const todos = read();
+  todos.forEach((currentTodo) => {
+    const isToUpdate = currentTodo.id == id;
+    if (isToUpdate) {
+      updatedTodo = Object.assign(currentTodo, partialTodo);
+    }
+  });
+
+  if (!updatedTodo) {
+    throw new Error("Id not found");
+  }
+
+  writeOnDB(todos);
+
+  return updatedTodo;
+}
+
+function CLEAR_DB() {
   fs.writeFileSync(DB_FILE_PATH, "");
 }
 
+function updateContentById(id: string, content: string): Todo {
+  return update(id, { content });
+}
+
 //SIMULATION
-clearDB();
-create("Segunda TODO");
+CLEAR_DB();
+create("Primeira TODO");
 create("Terceira TODO");
-console.log(read());
+const { id: secondId } = create("Segunda TODO");
+updateContentById(secondId, "New content");
+deleteById(secondId);
+console.log(read().length);
